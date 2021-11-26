d4f0e22666276d5fcaa12e9d0a9f28c6c094a592
Nek0-hinata <1160578676@qq.com>
2021-11-26 17:45:55 +0800

    照片选择页面美化完成

--- pages/select/select.js
+++ pages/select/select.js
@@ -1,8 +1,50 @@
 Page({
     data: {
-        change: 'outline'
+        portraitUrl: '/statics/add2.png',
+        bgUrl: '/statics/add1.png',
+        Selected: [
+            false,
+            false,
+            false,
+            false
+        ],
+        isSelectColor: '#AF601A',
+        isNotSelectColor: '#fdd62f'
     },
     onLoad: function (options) {
 
+    },
+
+    SelectPhoto: function (e) {
+        let that = this
+        wx.chooseImage({
+            count: 1,
+            sizeType: ['original'],
+            success(res) {
+                const tempFiles = res.tempFilePaths
+                if (e.mark.style == 0) {
+                    that.setData({
+                        portraitUrl: tempFiles
+                    })
+                } else {
+                    that.setData({
+                        bgUrl: tempFiles
+                    })
+                }
+            }
+        })
+    },
+
+    SelectHat: function (e) {
+        let that = this
+        let id = e.mark.hat
+        let temp = that.data.Selected
+        for (let i in temp) {
+            console.log(id)
+            temp[i] = i === id;
+        }
+        this.setData({
+            Selected: temp
+        })
     }
 });
